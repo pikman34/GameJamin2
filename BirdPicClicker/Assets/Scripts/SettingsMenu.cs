6cf7f55f7fb446745f44ad4219c37d10d3a896ba
amy <C24399391@myTUDublin.ie>
2025-03-06 23:19:55 +0000

    settings/pause menu

---
+++ BirdPicClicker/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUi;
+
+    private bool insideTrigger;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (insideTrigger && Input.GetMouseButtonDown((int)MouseButton.Left))
+        {
+            if (GameIsPaused)
+                {
+                    Resume();
+                } else
+                {
+                    Pause();
+                }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading Menu...");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Exiting Game...");
+        Application.Quit();
+    }
+}
+
