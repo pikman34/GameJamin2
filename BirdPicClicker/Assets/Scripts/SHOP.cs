9c1cf4209e5829a1e6088777b6e26111db419f59
Noah <noahbob34@gmail.com>
2025-03-09 03:00:08 +0000

    stuff ive been working on for the past few hours

--- BirdPicClicker/Assets/Scripts/SHOP.cs
+++ BirdPicClicker/Assets/Scripts/SHOP.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class SHOP : MonoBehaviour
 {
@@ -10,8 +11,11 @@ public class SHOP : MonoBehaviour
     public TMP_Text costText, nameText, numberText;
     public ResourceTracker myResources;
     public float autoClickIncrease = 0.1f;
+    public bool flash, tripod, hdlens, viewfinder, procam, birdfeed, flowers, birdbath, birdbath2, pond, pond2, birdhouse, birdhouse2, fatball, fatball2, mixedseed, mixedseed2, safflower, safflower2, meatworm, meatworm2, sunflower, sunflower2;
 
-
+    public Button safflowerbutton, meatbutton, sunflowerbutton, robinbutton, dovebutton, goldbutton, swanbutton, bluetitbutton;
+    public SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
     private void Start()
     {
         SetUI();
@@ -34,10 +38,111 @@ public class SHOP : MonoBehaviour
     {
         if (myResources.CheckResources(purchaseCost))
         {
-            myResources.RemoveResources(purchaseCost);
-            myResources.autoClicks += autoClickIncrease;
-            numberOwned++;
-            SetUI();
+            if (flash)
+            {
+                gameObject.SetActive(true); 
+                myResources.RemoveResources(purchaseCost);
+                myResources.autoClicks += autoClickIncrease;
+                numberOwned++;
+                SetUI();
+            }
+
+            if (tripod)
+            {
+                gameObject.SetActive(true);
+                myResources.RemoveResources(purchaseCost);
+                myResources.autoClicks += autoClickIncrease;
+                numberOwned++;
+                SetUI();
+            }
+
+            if (hdlens)
+            {
+                gameObject.SetActive(true);
+                myResources.RemoveResources(purchaseCost);
+                myResources.autoClicks += autoClickIncrease;
+                numberOwned++;
+                SetUI();
+            }
+
+            if (viewfinder)
+            {
+                gameObject.SetActive(true);
+                myResources.RemoveResources(purchaseCost);
+                myResources.autoClicks += autoClickIncrease;
+                numberOwned++;
+                SetUI();
+            }
+
+            if (procam)
+            {
+                gameObject.SetActive(true);
+                myResources.RemoveResources(purchaseCost);
+                myResources.autoClicks += autoClickIncrease;
+                numberOwned++;
+                SetUI();
+            }
+
+
+            if (birdfeed)
+            {
+                gameObject.SetActive(true);
+                myResources.RemoveResources(purchaseCost);
+                myResources.autoClicks += autoClickIncrease;
+                numberOwned++;
+                SetUI();
+            }
+
+            if (flowers)
+            {
+                gameObject.SetActive(true);
+                myResources.RemoveResources(purchaseCost);
+                myResources.autoClicks += autoClickIncrease;
+                numberOwned++;
+                SetUI();
+            }
+
+            if (birdbath)
+            {
+                safflowerbutton.interactable = true;
+                gameObject.SetActive(true);
+                myResources.RemoveResources(purchaseCost);
+                myResources.autoClicks += autoClickIncrease;
+                numberOwned++;
+                SetUI();
+            }
+
+            if (birdbath2)
+            {
+                gameObject.SetActive(false);
+            }
+
+            if (pond)
+            {
+                ChangeSprite();
+                meatbutton.interactable = true;
+                myResources.RemoveResources(purchaseCost);
+                myResources.autoClicks += autoClickIncrease;
+                numberOwned++;
+                SetUI();
+            }
+
+            if (birdhouse)
+            {
+                gameObject.SetActive(true);
+                bluetitbutton.interactable = true;
+                myResources.RemoveResources(purchaseCost);
+                myResources.autoClicks += autoClickIncrease;
+                numberOwned++;
+                SetUI();
+            }
         }
+
+    void ChangeSprite() 
+    {
+        spriteRenderer.sprite = newSprite;
+    }
+
+
     }
 }
